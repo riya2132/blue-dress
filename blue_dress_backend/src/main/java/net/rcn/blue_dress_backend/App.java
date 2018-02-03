a75e3504222e955be03c7685133bceea160b400d
riya verma <riyavera2132@gmail.com>
2018-02-03 18:56:18 +0530

    my project setup completed successfully

---
+++ blue_dress_backend/src/main/java/net/rcn/blue_dress_backend/App.java
@@ -0,0 +1,13 @@
+package net.rcn.blue_dress_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
